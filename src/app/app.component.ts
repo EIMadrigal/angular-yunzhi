d5b29cc46ace767f7c29b8359ef6789c9f4699b3
EI Madrigal <jingtaor@JingtaodeMacBook-Air.local>
2024-12-06 22:51:25 +0800

    initial hello world

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
+
+  constructor(private httpClient: HttpClient) {
+    httpClient.get('http://localhost:8080/helloWorld').subscribe((data) => {
+      console.log('Request successful');
+      console.log(data);
+      this.title = data as string;
+    }, error);
+  }
+
   title = 'angular-yunzhi';
+}
+
+function success(data: any) {
+  
+}
+
+function error(data: any) {
+  console.log('Request failed');
+  console.log(data);
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule
   ],
   providers: [
-    provideClientHydration()
+    provideHttpClient()
   ],
   bootstrap: [AppComponent]
 })
