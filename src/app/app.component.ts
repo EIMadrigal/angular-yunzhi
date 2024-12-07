f05cdaeb6b253ac2caf5346cf5026c69d6160ff3
EI Madrigal <jingtaor@JingtaodeMacBook-Air.local>
2024-12-07 21:12:25 +0800

    get data from backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,33 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   constructor(private httpClient: HttpClient) {
     
   }
 
-  title = 'angular-yunzhi';
+  teachers = new Array();
 
-  teachers = new Array(
-    {
-      id: 1,
-      name: 'San Zhang',
-      username: 'sanz',
-      email: 'San.Zhang@google.com',
-      gender: 'M'
-    },
-    {
-      id: 2,
-      name: 'Si Li',
-      username: 'sil',
-      email: 'Si.Li@alibaba.com',
-      gender: 'F'
+  ngOnInit(): void {
+    const url = 'http://localhost:8080/teacher';
+
+    const success = (response: any) => {
+      console.log(response);
+      this.teachers = response;
     }
-  );
+
+    const error = function(response: object) {
+      console.log(response);
+      console.error('Request failed');
+    }
+
+    this.httpClient.get(url).subscribe(success, error);
+  }
+
 }
