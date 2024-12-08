9dd28b5a436008ebfafd83cc93b158c7261afc46
EI Madrigal <jingtaor@JingtaodeMacBook-Air.local>
2024-12-08 10:13:15 +0800

    post form to backend

--- src/app/teacher-add.component.ts
+++ src/app/teacher-add.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
 
 @Component({
@@ -10,8 +11,31 @@ export class TeacherAddComponent implements OnInit {
     username!: string;
     email!: string;
     gender!: boolean;
+
+    constructor(private httpClient: HttpClient) {
+
+    }
     
     ngOnInit(): void {
 
+    }
+
+    public onSubmit() : void {
+        console.log('click submit button');
+        const url = 'http://localhost:8080/teacher';
+        const teacher = {
+            name: this.name,
+            username: this.username,
+            email: this.email,
+            gender: this.gender
+        };
+
+        console.log(teacher);
+
+        this.httpClient.post(url, teacher).subscribe((response) => {
+            console.log('request success');
+        }, (response) => {
+            console.error('request fail', response);
+        });
     }
 }
