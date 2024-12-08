3f23eb6cad2cb6315d3608b5370a1511f57238e4
EI Madrigal <jingtaor@JingtaodeMacBook-Air.local>
2024-12-08 16:41:47 +0800

    update and submit data

--- src/app/teacher-edit.component.ts
+++ src/app/teacher-edit.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { AppComponent } from "./app.component";
 
 @Component({
     templateUrl: './teacher-edit.component.html'
@@ -8,23 +9,36 @@ import { ActivatedRoute } from "@angular/router";
 export class TeacherEditComponent implements OnInit {
 
     public teacher: any = {};
+    private url!: string;
 
-    constructor(private route: ActivatedRoute, private httpClient: HttpClient) {
+    constructor(private route: ActivatedRoute, private httpClient: HttpClient, private appComponent: AppComponent) {
 
     }
 
+    getUrl(): string {
+        if (this.url === undefined) {
+            const id = this.route.snapshot.paramMap.get('id');
+            console.log(id);
+            this.url = 'http://localhost:8080/teacher/' + id;        
+        }
+        return this.url;
+    }
+
     ngOnInit(): void {
-        const id = this.route.snapshot.paramMap.get('id');
-        console.log(id);
-        const url = 'http://localhost:8080/teacher/' + id;
-        this.httpClient.get(url).subscribe((data) => {
+        this.httpClient.get(this.getUrl()).subscribe((data) => {
             this.teacher = data;
         }, () => {
-            console.log(`Request fail to ${url}`);
+            console.log(`Request fail to ${this.getUrl()}`);
         });
     }
 
     onSubmit() : void {
         console.log('submit');
+        this.httpClient.put(this.getUrl(), this.teacher).subscribe((data) => {
+            console.log('update success');
+            this.appComponent.ngOnInit();
+        }, () => {
+            console.log(`Request fail to ${this.getUrl()}`);
+        });
     }
 }
