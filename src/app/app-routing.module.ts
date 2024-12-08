7b7722db5c35acfe5f3124fd67f032bab27e8910
EI Madrigal <jingtaor@JingtaodeMacBook-Air.local>
2024-12-08 09:29:12 +0800

    add component teacher-add and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TeacherAddComponent } from './teacher-add.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'add',
+    component: TeacherAddComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/teacher-add.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: '',
+    templateUrl: './teacher-add.component.html'
+})
+export class TeacherAddComponent implements OnInit {
+    ngOnInit(): void {
+        
+    }
+}
