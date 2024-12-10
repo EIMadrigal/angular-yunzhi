17ad10612b2681ec7f8922068286e4eda4f80a68
EI Madrigal <andrew_ren@163.com>
2024-12-10 14:25:53 +0800

    create klass module

---
+++ src/app/klass/klass.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { IndexComponent } from './index/index.component';
+
+@NgModule({
+  declarations: [
+    IndexComponent
+  ]
+})
+export class KlassModule {
+
+}
